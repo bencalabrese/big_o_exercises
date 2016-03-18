e5ce1f88a3ab706207c65bdf9c07bd76a4f82bd9
Ben Calabrese <bencalabrese@gmail.com>
2016-03-17 18:01:05 -0700

    finished max windowed range

--- max_windowed_range/exercise.rb
+++ max_windowed_range/exercise.rb
@@ -1,8 +1,8 @@
 require 'byebug'
-
+require_relative 'my_stackqueue'
 
 # O(n^2)
-def windowed_max_range(arr, window_size)
+def bad_windowed_max_range(arr, window_size)
   biggest_max = 0
 
   arr.take(arr.length - window_size + 1).each_index do |index|
@@ -13,10 +13,25 @@ def windowed_max_range(arr, window_size)
   biggest_max
 end
 
+# O(n)
+def good_windowed_max_range(arr, window_size)
+  queue = StackQueue.new
+
+  arr.take(window_size).each { |el| queue.enqueue(el) }
+  biggest_max = queue.max - queue.min
+
+  arr.drop(window_size).each do |el|
+    queue.dequeue
+    queue.enqueue(el)
+    current_max = queue.max - queue.min
+    biggest_max = current_max if current_max > biggest_max
+  end
+
+  biggest_max
+end
 
 
-
-# p windowed_max_range([1, 0, 2, 5, 4, 8], 2) == 4 # 4, 8
-# p windowed_max_range([1, 0, 2, 5, 4, 8], 3) == 5 # 0, 2, 5
-# p windowed_max_range([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
-# p windowed_max_range([1, 3, 2, 5, 4, 8], 5) == 6 # 3, 2, 5, 4, 8
+p good_windowed_max_range([1, 0, 2, 5, 4, 8], 2) == 4 # 4, 8
+p good_windowed_max_range([1, 0, 2, 5, 4, 8], 3) == 5 # 0, 2, 5
+p good_windowed_max_range([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
+p good_windowed_max_range([1, 3, 2, 5, 4, 8], 5) == 6 # 3, 2, 5, 4, 8
